7be01e422687c5f3e80a98c1ded81814130f7020
d-zucho <danny.nico.dev@outlook.com>
2024-12-09 17:38:04 -0800

    sync neon table and prisma schema

---
+++ app/utils/db.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import { PrismaClient } from '@prisma/client'
+// const PrismaClient = require('@prisma/client').PrismaClient
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
