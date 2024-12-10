4b2dc404d393e44bd7e18cea269fc1ab1895724b
d-zucho <danny.nico.dev@outlook.com>
2024-12-09 16:36:39 -0800

    add nodemailer initial instalation with mailtrap

---
+++ auth.ts
@@ -0,0 +1,20 @@
+import Nodemailer from 'next-auth/providers/nodemailer'
+import NextAuth from 'next-auth'
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Nodemailer({
+      server: {
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+      },
+    }),
+  ],
+})
